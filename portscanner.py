cfd509620aea05cfa5482d1becfdba3267d2b8a4
Arthur Albuquerque <80676157+arthur-albuquerque1@users.noreply.github.com>
2024-01-02 14:26:32 -0300

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -3,8 +3,10 @@ import sys
 import socket
 
 #Definindo o alvo
-if len(sys.argv) == 2:
+if len(sys.argv) == 4:
 	target = socket.gethostbyname(sys.argv[1]) #Endereço IPv4 do alvo a ser escaneado
+	port1 = int(sys.argv[2]) #Define o início do range de portas TCP a serem escaneadas
+	port2 = int(sys.argv[3]) #Define o fim do range de portas TCP a serem escaneadas
 else:
 	print('Quantidade de argumentos inválida') #Erro caso a quantidade de argumentos esteja errada, 2 argumentos são necessários para o código funcionar: portscanner.py + IP_DO_ALVO
 
@@ -15,7 +17,7 @@ print("Início: "+str(datetime.now()))
 print("-" * 50)
 
 try: #Processo de escaneio de cada uma das portas TCP do alvo
-	for port in range(20,80): #Define a faixa de portas TCP que deve ser escaneada
+	for port in range(port1,port2): #Define a faixa de portas TCP que deve ser escaneada
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Utiliza o módulo socket para determinar o IP e as portas TCP
 		socket.setdefaulttimeout(1) #Define o tempo máximo que cada porta pode ser escaneada, nesse caso 1 segundo
 		result = s.connect_ex((target,port)) #Define os resultados
