b34b05821fbfaf07740805fc0580f5247bbb47ad
Arthur Albuquerque <80676157+arthur-albuquerque1@users.noreply.github.com>
2024-01-03 22:51:32 -0300

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -1,39 +1,66 @@
+#!/bin/python3
+
 from datetime import datetime
 import sys
 import socket
 
-#Definindo o alvo
-if len(sys.argv) == 4:
-	target = socket.gethostbyname(sys.argv[1]) #Endereço IPv4 do alvo a ser escaneado
-	port1 = int(sys.argv[2]) #Define o início do range de portas TCP a serem escaneadas
-	port2 = int(sys.argv[3]) #Define o fim do range de portas TCP a serem escaneadas
-else:
-	print('Quantidade de argumentos inválida') #Erro caso a quantidade de argumentos esteja errada, 2 argumentos são necessários para o código funcionar: portscanner.py + IP_DO_ALVO
+def visual_lines():
+    print('-' * 50)
 
-#Adicionando um banner simples para criar um efeito gráfico
-print("-" * 50)
-print("Escaneando o endereço: "+target)
-print("Início: "+str(datetime.now()))
-print("-" * 50)
+def banner(target):
+    visual_lines()
+    print(f"Escaneando o endereço: {target}")
+    print(f"Início: {str(datetime.now())}")
+    visual_lines()
 
-try: #Processo de escaneio de cada uma das portas TCP do alvo
-	for port in range(port1,port2): #Define a faixa de portas TCP que deve ser escaneada
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Utiliza o módulo socket para determinar o IP e as portas TCP
-		socket.setdefaulttimeout(1) #Define o tempo máximo que cada porta pode ser escaneada, nesse caso 1 segundo
-		result = s.connect_ex((target,port)) #Define os resultados
-		if result == 0: 
-			print(f'Porta {port} está aberta') #Caso o resultado seja igual a 0, então a porta está aberta
-		s.close()
+def scan(target, port1, port2):
+    try:
+        for port in range(port1, port2 + 1):
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(0.1)
 
-except KeyboardInterrupt: #Interrupção realizada pelo usuário para sair do programa, CTRL + C
-	print('\nSaindo do programa')
-	sys.exit()
+            result = s.connect_ex((target, port))
 
-except socket.gaierror: #Erro padrão do módulo socket, caso o endereço IP não seja encontrado
-	print('Nome do Host não pôde ser definido')
-	sys.exit()
-	
-except socket.error: #Erro padrão do módulo socket
-	print('Não foi possível se conectar ao servidor')
-	sys.exit()
+            if result == 0:
+                print(f'A porta {port} está aberta')
+            s.close()
+    except KeyboardInterrupt:
+        print('\nSaindo do programa')
+        sys.exit()
+    except socket.gaierror:
+        print('Nome do Host não pôde ser definido')
+        sys.exit()
+    except socket.error:
+        print('Não foi possível se conectar ao servidor')
+        sys.exit()
 
+def main():
+    visual_lines()
+    print('|0|: Unico endereço')
+    print('|1|: Lista de endereços')
+    mode = int(input('Selecione o modo: '))
+
+    if mode == 0:
+        visual_lines()
+        target = socket.gethostbyname(input('Digite o endereço do alvo: '))
+        port1 = int(input('Digite a primeira porta TCP: '))
+        port2 = int(input('Digite a ultima porta TCP: '))
+        banner(target)
+        scan(target, port1, port2)
+
+    elif mode == 1:
+        filename = input('Digite o nome do arquivo: ')
+        with open(filename) as archive:
+            ip_list = archive.read().splitlines()
+        port1 = int(input('Digite a primeira porta TCP: '))
+        port2 = int(input('Digite a ultima porta TCP: '))
+        for ip in ip_list:
+            target = ip
+            banner(target)
+            scan(target, port1, port2)
+
+    else:
+        print('Selecione um modo válido')
+
+if __name__ == "__main__":
+    main()
